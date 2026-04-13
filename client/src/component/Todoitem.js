e9965f9d9bf773debb6da42f790d8bb9515d8054
Ahmed-Belgacem <ahmedbelgacem167@gmail.com>
2026-04-13 12:05:07 +0000

    12th commit

--- client/src/component/Todoitem.js
+++ client/src/component/Todoitem.js
@@ -6,13 +6,13 @@ import "./admin.css";
 const Todoitem = ({ todo }) => {
     const dispatch = useDispatch();
   return (
-    <div className={`todo-item ${todo.isDone ? "undone" : "done"}`}>
+    <div className={`todo-item ${todo.isDone ? "done" : "undone"}`}>
       <div className='text'>
         <h2>{todo.title}</h2>
         <p>{todo.description}</p>
       </div>
       <span onClick={() => dispatch(doneTask({ id: todo.id }))}>
-         {todo.isDone ? "done" : "undone"}
+         {todo.isDone ? "undone" : "done"}
       </span>
       <Edittask todo={todo} />
       <button onClick={() => dispatch(removeTask({ id: todo.id }))}>Remove</button>
